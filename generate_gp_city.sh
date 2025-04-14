ee4f7c02930182bac819109b5af5fedc2c83cf45
edrian-liao <edrianpaul.liao@duke.edu>
2025-04-14 10:34:58 -0400

    cities

--- generate_gp_city.sh
+++ generate_gp_city.sh
@@ -12,14 +12,18 @@
 source ~/.bashrc
 conda activate wildfire-ai
 
+python generate_gp.py --city "Atlanta" --resolution 500 --num_points 4
 python generate_gp.py --city "Baltimore_A" --resolution 500 --num_points 4
 python generate_gp.py --city "Baltimore_B" --resolution 500 --num_points 4
 python generate_gp.py --city "Boston" --resolution 500 --num_points 4
 python generate_gp.py --city "Chicago" --resolution 500 --num_points 4
+python generate_gp.py --city "Detroit" --resolution 500 --num_points 4
 python generate_gp.py --city "Durham" --resolution 500 --num_points 4
 python generate_gp.py --city "Houston" --resolution 500 --num_points 4
 python generate_gp.py --city "Kansas" --resolution 500 --num_points 4
-python generate_gp.py --city "Detroit" --resolution 500 --num_points 4
+python generate_gp.py --city "Las Vegas" --resolution 500 --num_points 4
+python generate_gp.py --city "Los Angeles" --resolution 500 --num_points 4
+python generate_gp.py --city "Miami" --resolution 500 --num_points 4
 python generate_gp.py --city "Nashville" --resolution 500 --num_points 4
 python generate_gp.py --city "New Orleans" --resolution 500 --num_points 4
 python generate_gp.py --city "NYC" --resolution 500 --num_points 4
